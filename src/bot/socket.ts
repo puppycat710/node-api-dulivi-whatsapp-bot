9b1422c5881763b4623c0a60d1e66690a22fb17e
puppycat710 <puppycat710@proton.me>
2025-09-23 19:17:24 -0300

    a3

--- src/bot/socket.ts
+++ src/bot/socket.ts
@@ -1,17 +1,14 @@
-import {
-  makeWASocket,
-  useMultiFileAuthState,
-} from "@whiskeysockets/baileys";
-import { initSocketEvents } from "./events";
+import { makeWASocket, useMultiFileAuthState } from '@whiskeysockets/baileys'
+import { initSocketEvents } from './events'
 
 export const initWASocket = async () => {
-  const { state, saveCreds } = await useMultiFileAuthState("auth");
+	const { state, saveCreds } = await useMultiFileAuthState('/opt/render/data/auth')
 
-  const sock = makeWASocket({
-    auth: state,
-    browser: ['Bot', 'Chrome', '1.0.0'],
-    printQRInTerminal: true
-  });
+	const sock = makeWASocket({
+		auth: state,
+		browser: ['Bot', 'Chrome', '1.0.0'],
+		printQRInTerminal: true,
+	})
 
-  initSocketEvents(sock, saveCreds);
-};
+	initSocketEvents(sock, saveCreds)
+}
