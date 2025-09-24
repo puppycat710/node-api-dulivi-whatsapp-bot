c06878389337e4081871f6fba691185a706e01d3
puppycat710 <puppycat710@proton.me>
2025-09-24 13:49:42 -0300

    formatJid

--- src/utils/formatJid.ts
+++ src/utils/formatJid.ts
@@ -1,11 +1,11 @@
-export function formatJid(number: string) {
-  // mantém só dígitos
-  let onlyNumbers = number.replace(/\D/g, '')
+export function formatJid(number: string): string {
+	// remove tudo que não é número
+	let onlyNumbers = number.replace(/\D/g, '')
 
-  // se não começar com 55, adiciona
-  if (!onlyNumbers.startsWith('55')) {
-    onlyNumbers = '55' + onlyNumbers
-  }
+	// se não começar com 55, adiciona
+	if (!onlyNumbers.startsWith('55')) {
+		onlyNumbers = '55' + onlyNumbers
+	}
 
-  return `${onlyNumbers}@s.whatsapp.net`
+	return `${onlyNumbers}@s.whatsapp.net`
 }
